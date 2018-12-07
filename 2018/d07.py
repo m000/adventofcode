aaed2a4ab1c8403840671c393ae231ff84604a8e
Manolis Stamatogiannakis <mstamat@gmail.com>
2018-12-07 19:03:38 +0100

    refactor/generalization for day 7 - 2018

--- 2018/d07.py
+++ 2018/d07.py
@@ -7,7 +7,64 @@ from pprint import pprint
 DEBUG = False
 INPUT = 'd07-input.txt'
 line_re = re.compile('Step (?P<s1>[a-zA-Z]+) must be finished (?P<when>[a-z]+) step (?P<s2>[a-zA-Z]+) can begin.')
-NWORKERS = 5
+duration = lambda s: 60 + ord(s) - ord('A') + 1
+
+# ------------------------------------------------
+# executes the steps with the provided config
+# ------------------------------------------------
+def work(steps, dependencies, nworkers):
+    steps = steps.copy()
+    dependencies = copy.deepcopy(dependencies)
+    workers = [-1,] * nworkers              # when the worker is next available
+    ongoing = [None,] * nworkers            # what the worker is working on
+    order = []
+    t = -1
+    updated = True
+
+    while not (not steps and ongoing.count(None) == nworkers):
+        # increment clock
+        t += 1
+
+        # dump state
+        if DEBUG and updated:
+            print(t, file=sys.stderr)
+            print(workers, file=sys.stderr)
+            print(ongoing, file=sys.stderr)
+            print(''.join(sorted(steps)), file=sys.stderr)
+            print(''.join([s if s not in dependencies else '*' for s in sorted(steps)]), file=sys.stderr)
+            print(dependencies, file=sys.stderr)
+            print("", file=sys.stderr)
+        updated = False
+
+        # check workers for finished steps
+        finished = set()
+        for i in range(nworkers):
+            if workers[i] <= t and ongoing[i] is not None:
+                finished.add(ongoing[i])
+                ongoing[i] = None
+
+        # update dependencies to unblock steps
+        if finished:
+            updated = True
+            unblocked = []
+            for s, d in dependencies.items():   # update dependencies
+                d -= finished
+                if not d:
+                    unblocked.append(s)
+            for s in unblocked:                 # collect garbage
+                dependencies.pop(s, None)
+
+        # assign available jobs
+        workers_available = [i for i in range(nworkers) if ongoing[i] is None and workers[i] <= t]
+        steps_available = [s for s in sorted(steps) if s not in dependencies]
+        for w, s in zip(workers_available, steps_available):
+            updated = True
+            workers[w] = t + duration(s)
+            ongoing[w] = s
+            order.append(s)
+            steps.remove(s)
+
+    return((''.join(order), t))
 
 # ------------------------------------------------
 # scan input
@@ -27,82 +84,14 @@ with open(INPUT) as f:
         dependencies[s2].add(s1)
         steps.update((s1, s2))
 
-# keep a copy of steps/deps for step 2
-dependencies_c = copy.deepcopy(dependencies)
-steps_c = steps.copy()
-
 # ------------------------------------------------
 # part 1
 # ------------------------------------------------
-nsteps = -1                             # guard against inf. loops
-order = []                              # order of steps
-while len(steps) > 0 and len(steps) != nsteps:
-    nsteps = len(steps)
-    next_step = set(sorted([s for s in steps if s not in dependencies or not dependencies[s]])[0])
-    order.extend(next_step)            # update order
-    steps -= next_step                 # update steps
-    garbage = []
-    for s, d in dependencies.items():  # update dependencies
-        d -= next_step
-        if not d:
-            garbage.append(s)
-    for s in garbage:                   # collect garbage
-        dependencies.pop(s, None)
-assert(not steps)
-print(''.join(order))
+print(work(steps, dependencies, 1))
 
 # ------------------------------------------------
 # part 2
 # ------------------------------------------------
-duration = lambda s: 60 + ord(s) - ord('A') + 1
-steps = steps_c
-dependencies = dependencies_c
-workers = [-1,] * NWORKERS              # when the worker is next available
-ongoing = [None,] * NWORKERS            # what the worker is working on
-t = -1
-updated = True
-
-while not (not steps and ongoing.count(None) == NWORKERS):
-    # increment clock
-    t += 1
-
-    # dump state
-    if DEBUG and updated:
-        print(t, file=sys.stderr)
-        print(workers, file=sys.stderr)
-        print(ongoing, file=sys.stderr)
-        print(''.join(sorted(steps)), file=sys.stderr)
-        print(''.join([s if s not in dependencies else '*' for s in sorted(steps)]), file=sys.stderr)
-        print(dependencies, file=sys.stderr)
-        print("", file=sys.stderr)
-    updated = False
-
-    # check workers for finished steps
-    finished = set()
-    for i in range(NWORKERS):
-        if workers[i] <= t and ongoing[i] is not None:
-            finished.add(ongoing[i])
-            ongoing[i] = None
-
-    # update dependencies to unblock steps
-    if finished:
-        updated = True
-        unblocked = []
-        for s, d in dependencies.items():   # update dependencies
-            d -= finished
-            if not d:
-                unblocked.append(s)
-        for s in unblocked:                 # collect garbage
-            dependencies.pop(s, None)
-
-    # assign available jobs
-    workers_available = [i for i in range(NWORKERS) if ongoing[i] is None and workers[i] <= t]
-    steps_available = [s for s in sorted(steps) if s not in dependencies]
-    for w, s in zip(workers_available, steps_available):
-        updated = True
-        workers[w] = t + duration(s)
-        ongoing[w] = s
-        steps.remove(s)
-print(t)
+print(work(steps, dependencies, 5))
 
 # vim:sts=4:sw=4:et:
