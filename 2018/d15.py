9a7f389f930e47697374a71f7db66cbb5fb7cdfa
Manolis Stamatogiannakis <mstamat@gmail.com>
2018-12-29 13:40:59 +0200

    day 15 - part 1 - 2018

--- 2018/d15.py
+++ 2018/d15.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 import re
 import sys
+import itertools
 import numpy as np
-from scipy.sparse import csr_matrix
-from scipy.sparse import csgraph
-from collections import Counter
+import networkx as nx
 from matplotlib import pyplot
 from pprint import pprint
-INPUTS = [ 'd15-input.txt', ]
+INPUTS = [ 'd15-input.txt',
+        'd15-input-example1.txt', 'd15-input-example2.txt', 'd15-input-example3.txt',
+        'd15-input-example4.txt', 'd15-input-example5.txt', 'd15-input-example6.txt',
+        'd15-input-test1.txt', 'd15-input-test2.txt', 'd15-input-test3.txt',
+        'd15-input-test4.txt', 'd15-input-test5.txt', 'd15-input-test6.txt',
+        'd15-input-test7.txt', ]
 
-DEBUG = True
-INPUT = INPUTS[0] if DEBUG else INPUTS[0]
+DEBUG = False
+DEBUG_STEP = False
+INPUT = INPUTS[13] if DEBUG else INPUTS[0]
 
 # ------------------------------------------------
 # constants and global state
@@ -21,44 +26,169 @@ units_hp = {'G': 200, 'E': 200}
 units_ap = {'G': 3, 'E': 3}
 units_id = {'G': iter(range(units_max)), 'E': iter(range(units_max))}
 
-carts_turn = {
-        '</':  'v', '>/':  '^', '^/':  '>', 'v/':  '<',
-        '<\\': '^', '>\\': 'v', '^\\': '<', 'v\\': '>',
-}
-carts_iturn = {
-        '<l': ('v', 's'), '>l': ('^', 's'), '^l': ('<', 's'), 'vl': ('>', 's'),
-        '<s': ('<', 'r'), '>s': ('>', 'r'), '^s': ('^', 'r'), 'vs': ('v', 'r'),
-        '<r': ('^', 'l'), '>r': ('v', 'l'), '^r': ('>', 'l'), 'vr': ('<', 'l'),
-}
-
 # ------------------------------------------------
 # helpers
 # ------------------------------------------------
+hr = lambda s: ('--- %s ' % s).ljust(70, '-')
+
 class Unit:
-    def __init__(self, upos, utype):
-        self.posx, self.posy = upos
+    def __init__(self, upos, utype, board):
+        self.board = board
+        self.i, self.j = upos
         self.type = utype
         self.hp = units_hp[self.type]
         self.ap = units_ap[self.type]
         self.id = next(units_id[self.type])
 
     def __str__(self):
-        return '%s%d[hp=%d, ap=%d, pos=(%d, %d)]' % (
-                self.type, self.id, self.hp, self.ap, self.posx, self.posy)
+        return '%s[%s, hp=%03d, ap=%d]' % (
+                self.uid, self.pos, self.hp, self.ap)
 
     def __repr__(self):
         return self.__str__()
 
     def __lt__(self, other):
-        return (self.posx, self.posy) < (other.posx, other.posy)
+        return (self.i, self.j) < (other.i, other.j)
 
     @property
     def uid(self):
         return '%s%d' % (self.type, self.id)
 
-    def move(self, board):
-        ir = board.in_range(for_type=self.type)
-        #print(ir)
+    @property
+    def pos(self):
+        return (self.i, self.j)
+
+    @property
+    def posxy(self):
+        return (self.j, self.i)
+
+    @property
+    def neighbors(self):
+        ''' Returns neighbouring tiles coordinates in reading order.
+            Neighbours returned are guaranteed to be available for movement.
+        '''
+        n = [ (self.i-1, self.j), (self.i, self.j-1), (self.i, self.j+1), (self.i+1, self.j), ]
+        return list(filter(self.board.is_open, n))
+
+    @property
+    def enemies(self):
+        return filter(lambda u: u.type != self.type, self.board.units)
+
+    def add_to_graph(self):
+        ''' Add the position of the unit to the graph as open tile.
+        '''
+        self.board.graph.add_node(self.pos) # needed in case unit is surrounded
+        for n in self.neighbors:
+            if self.board.typeof(n) != '#':
+                self.board.graph.add_edge(self.pos, n, weight=1)
+
+    def remove_from_graph(self):
+        ''' Remove the position of the unit from the graph.
+        '''
+        self.board.graph.remove_node(self.pos)
+
+    def get_targets(self):
+        ''' Returns the movement and attack targets for this unit.
+            Movement targets are tiles that have an enemy in attack range.
+            Attack targets are enemy units currently in range.
+        '''
+        move_targets = set()
+        attack_targets = []
+
+        # temporarily remove us from board
+        # this allows our current tile to be listed as neighbor of enemies
+        self.board.board[self.i, self.j] = '.'
+
+        for e in self.enemies:
+            en = list(e.neighbors)
+            move_targets.update(en)
+            if self.pos in en:
+                attack_targets.append(e)
+
+        self.board.board[self.i, self.j] = self.type
+        return (move_targets, attack_targets)
+
+    def move(self, move_to):
+        print('%s moved to %s' % (self, move_to), file=sys.stderr)
+
+        # make sure node is in the graph        
+        if self.pos not in self.board.graph.nodes:
+            self.add_to_graph()
+
+        # mark position as open on the board
+        self.board.board[self.i, self.j] = '.'
+
+        # move to new position and update graph/board
+        self.i, self.j = move_to
+        self.remove_from_graph()
+        self.board.board[self.i, self.j] = self.type
+
+    def attack(self, other):
+        other.hp -= self.ap
+        if other.hp <= 0:
+            print('%s killed %s' % (self, other), file=sys.stderr)
+
+            # add unit to graph as open tile
+            assert(other.pos not in self.board.graph.nodes)
+            other.add_to_graph()
+
+            # remove unit from board and units list
+            self.board.board[other.i, other.j] = '.'
+            self.board.units.remove(other)
+        else:
+            print('%s attacked %s' % (self, other), file=sys.stderr)
+
+    def action(self):
+        # add to board and compute all distances to open tiles
+        self.add_to_graph()
+        dists = nx.single_source_shortest_path_length(self.board.graph, self.pos)
+
+        # get move/attack targets
+        move_targets, attack_targets = self.get_targets()
+        if attack_targets:
+            hpmin = min([e.hp for e in attack_targets])
+            tgt_a = min([e for e in attack_targets if e.hp == hpmin])
+            self.attack(tgt_a)
+            self.remove_from_graph()
+            return
+
+        # keep only target tiles in distances dict
+        dists = { p: d for p, d in dists.items() if p in move_targets }
+
+        # blocked
+        if not dists:
+            print('%s skipped turn' % self, file=sys.stderr)
+            self.remove_from_graph()
+            return
+
+        # find the minimum distance
+        mindist = min(dists.values())
+
+        # keep only target tiles with the minimum distance
+        dists = { p: d for p, d in dists.items() if d == mindist }
+
+        # get the first target tile in reading order
+        tgt_m = min(dists)
+
+        # there may be different paths to the target tile!
+        tgt_path = None
+        for n in self.neighbors:
+            p = nx.shortest_path(self.board.graph, n, tgt_m)
+            if len(p) == mindist:
+                tgt_path = p
+                break
+        assert(tgt_path is not None)
+
+        # move - no need to remove from board
+        self.move(tgt_path[0])
+
+        # if mindist was 1, we are now in range
+        if mindist == 1:
+            move_targets, attack_targets = self.get_targets()
+            hpmin = min([e.hp for e in attack_targets])
+            tgt_a = min([e for e in attack_targets if e.hp == hpmin])
+            self.attack(tgt_a)
+            return
 
 class Board:
     def __init__(self, inputf):
@@ -76,43 +206,62 @@ class Board:
             for ln, line in enumerate(f):
                 line = line.rstrip()
                 if not line.lstrip(): continue
+                elif line.lstrip().startswith('//'): continue
                 self.board[ln] = list(line.ljust(self.dims[1]))
                 for um in re.finditer(units_re, line):
-                    upos, utype = (um.span()[0], ln), um.group(0)
-                    u = Unit(upos, utype)
-                    self.board[u.posy, u.posx] = '.'
+                    upos, utype = (ln, um.span()[0]), um.group(0)
+                    u = Unit(upos, utype, self)
+                    self.board[u.i, u.j] = u.type
                     self.units.append(u)
         self.units.sort()
 
+        # initialize graph
+        # Note: SciPy also has support for shortest path algorithms.
+        #       However, it requires explicit mapping of nodes to arithmetic
+        #       labels, and representing the graph as a NxN csr matrix.
+        #       Updating the matrix is also a pain because of the explicit
+        #       mapping to arithmetic labels.
+        #       Networkx makes it much simpler to maintain a consistent
+        #       state of the simulation.
+        self.init_graph()
+
         # initialize other state
-        self.ts = 0
-        self.inrange_cache = {'ts': -1}
+        self.round = 0
+
+    def typeof(self, p):
+        ''' Returns type of tile in coords p.
+        '''
+        return self.board[p[0], p[1]]
+
+    def in_bounds(self, p):
+        ''' Returns if coords p are in bounds.
+        '''
+        return (0 <= p[0] < self.dims[0]) and (0 <= p[1] < self.dims[1])
+
+    def is_open(self, p):
+        ''' Returns if coords p are in bounds and open.
+        '''
+        return self.in_bounds(p) and self.typeof(p) == '.'
+
+    def init_graph(self):
+        self.graph = nx.Graph()
+        neighbors = ((-1, 0), (0, -1), (0, 1), (1, 0))
+
+        for i, j in itertools.product(range(self.dims[0]), range(self.dims[1])):
+            p = (i, j)
+            if self.board[i, j] != '.':
+                continue
+            for di, dj in neighbors:
+                n = (i + di, j + dj)
+                if self.in_bounds(n) and self.typeof(n) == '.':
+                    self.graph.add_edge(p, n, weight=1)
 
     def __repr__(self):
         return '%s<%dx%d, %d units: %s>' % (self.__class__.__name__,
                 self.dims[1], self.dims[0], len(self.units), self.units)
 
     def __str__(self):
-        return self.tostring()
-
-    def overlay(self, items, f = None):
-        if f is not None:
-            items = map(f, items)
-        replaced = []
-        for pos, o, valid in items:
-            v = self.board[pos[0], pos[1]]
-            assert(v == valid or valid is None)
-            self.board[pos[0], pos[1]] = o
-            replaced.append( (pos, v, o) )
-        return replaced
-
-    def tostring(self, overlay_units=True):
-        if overlay_units:
-            ubak = self.overlay(self.units, lambda u: ((u.posy, u.posx), u.type, '.'))
-        s = '\n'.join(np.apply_along_axis(lambda s: ''.join(s), axis=1, arr=self.board))
-        if overlay_units:
-            self.overlay(ubak)
-        return s
+        return '\n'.join(np.apply_along_axis(lambda s: ''.join(s), axis=1, arr=self.board))
 
     def plot(self):
         # https://matplotlib.org/examples/color/colormaps_reference.html
@@ -121,156 +270,68 @@ class Board:
         Ecolor = 66     # elf
         Gcolor = 110    # gnome
         rcolor = 220    # in range
-        cprops = {
-                'color': 'white',
-                'fontweight': 'black',
-                'fontfamily': 'monospace',
-                'horizontalalignment': 'center',
-                'verticalalignment': 'center',
-        }
+
         imdata = np.full(self.board.shape, wcolor, dtype=np.uint8)
         #----------------------
         coords = np.where(self.board == '.')
         imdata[coords] = scolor
         #----------------------
-        inrange = self.in_range()
-        coords = tuple(zip(*inrange.keys()))
+        inrange = set()
+        for u in self.units:
+            inrange.update(u.neighbors)
+        coords = tuple(zip(*inrange))
         imdata[coords] = rcolor
         #----------------------
-        coords = tuple(zip(*[(u.posy, u.posx) for u in self.units if u.type == 'G']))
+        coords = np.where(self.board == 'G')
         imdata[coords] = Gcolor
         #----------------------
-        coords = tuple(zip(*[(u.posy, u.posx) for u in self.units if u.type == 'E']))
+        coords = np.where(self.board == 'E')
         imdata[coords] = Ecolor
         #----------------------
         img = pyplot.imshow(imdata, cmap='Paired')
+        img.get_figure().suptitle(self.score, fontfamily='monospace')
         pyplot.show()
 
-    def in_range(self, for_type=None):
-        for_type = for_type if for_type is not None else 'all'
-        if self.inrange_cache['ts'] == self.ts:
-            return self.inrange_cache[for_type]
+    @property
+    def finished(self):
+        return len(set([u.type for u in self.units])) < 2
 
-        self.inrange_cache = {'ts': self.ts, 'G': {}, 'E': {}, 'all': {}}
+    @property
+    def score(self):
+        s = {}
         for u in self.units:
-            neighbors = [(u.posy+1, u.posx), (u.posy-1, u.posx), (u.posy, u.posx+1), (u.posy, u.posx-1)]
-            for p in neighbors:
-                if self.board[p[0], p[1]] != '.': continue
-                for ft, cache in self.inrange_cache.items():
-                    if ft == u.type: continue
-                    if type(cache) != dict: continue
-                    if p not in cache: cache[p] = []
-                    cache[p].append(u.uid)
-        return self.inrange_cache[for_type]
+            if u.type not in s:
+                s[u.type] = 0
+            s[u.type] += u.hp
+        return '\n'.join([ '%s:[%3d x %4d = %6d]' % (k, self.round, v, v*self.round) for k, v in s.items()])
 
     def tick(self):
-        # set unit locations as walls
-        ubak = self.overlay(self.units, lambda u: ((u.posy, u.posx), '#', '.'))
+        print(hr('round %04d' % self.round), file=sys.stderr)
 
-        # We want scipy to compute shortest paths. For this, we need
-        # to express the map as a NxN sparse matrix. The value at
-        # (i, j) represents the direct distance between points labeled
-        # i and j. 
-        # For this, we need to extract the points we want, assign
-        # them labels, create a sparse matrix and feed it to scipy.
+        # sort units
+        self.units.sort()
 
-        # make labels and sparse matrix indices
-        coords_label = iter(range(self.dims[0] * self.dims[1]))
-        coords = {}; labels = {}; csr_row = []; csr_col = []
-        for i in np.arange(self.dims[0]-1):      # skip last row
-            for j in np.arange(self.dims[1]-1):  # skip last column
-                if self.board[i, j] != '.': continue
-
-                # find neighbors
-                # undirected graph - only need to check 2 neighbors
-                neighbors = []
-                if self.board[i, j+1] == '.':
-                    neighbors.append((i, j+1))
-                if self.board[i+1, j] == '.':
-                    neighbors.append((i+1, j))
-                if not neighbors: continue
-
-                # set labels and distances
-                p = (i, j)
-                if p not in labels:
-                    lp = next(coords_label)
-                    labels[p] = lp
-                    coords[lp] = p
-                else:
-                    lp = labels[p]
-                for n in neighbors:
-                    if n not in labels:
-                        ln = next(coords_label)
-                        labels[n] = ln
-                        coords[ln] = n
-                    else:
-                        ln = labels[n]
-                    csr_row.append(lp)
-                    csr_col.append(ln)
-
-        # create sparse matrix
-        csr_dim = (len(csr_row), len(csr_col))
-        csr_data = [1,] * len(csr_row)
-        assert(csr_dim[0] == csr_dim[1])
-        m = csr_matrix((csr_data, (csr_row, csr_col)), shape=csr_dim, dtype=np.uint8)
-        print(len(csr_data))
-
-        # calculate shortest distances
-        dists, pred = csgraph.shortest_path(m, method='D',
-                directed=False, unweighted=True, return_predecessors=True)
-        dists_nonzero = np.logical_and(np.isfinite(dists), dists > 0)
-        for i, j in zip(*np.where(dists_nonzero)):
-            print('%02d: %d%s -> %d%s' % (dists[i, j], i, coords[i], j, coords[j]))
-
-        start = 380
-        dest = 366
-        while dists[dest, start] > 0:
-            n = pred[dest, start]
-            print('%d%s -> %d%s' % (start, coords[start], n, coords[n]))#, dists[n, dest]))
-            start = n
-
-        #02: 380(30, 11) -> 373(30, 9)
-        # 10: 380(30, 11) -> 376(30, 15)
-        print(pred[380, 380])
-        #print(sh[np.isfinite(sh)])
-        #for i in np.arange(sh.shape[0]):
-            #for j in np.arange(sh.shape[1]):
-        
-        #print(self.tostring(overlay_units = False))
-        self.overlay(ubak)
-
-        ## calculate coordinates of spaces
-        ##scoords = set(zip(*np.where(self.board == '.')))
-        #ucoords = set([(u.posy, u.posx) for u in self.units])
-        #scoords -= ucoords
-        #scoords = dict(enumerate(sorted(scoords)))
-        #print(scoords)
-
-
-
-        #for u in self.units:
-            #u.move(self)
-
-
-
-# ------------------------------------------------
-# read board
-# ------------------------------------------------
-b = Board(INPUT)
-print(b)
-print(repr(b))
-b.tick()
-b.plot()
-
-
-#if DEBUG:
-    #print_board(board, units)
-    #plot_board(board, units)
-sys.exit(0)
+        # activate units - use a copy as units may die while looping
+        for u in self.units.copy():
+            if (u.hp <= 0):
+                continue
+            if self.finished:
+                self.plot()
+                return
+            if DEBUG_STEP:
+                self.plot()
+            u.action()
+        self.plot()
+        self.round += 1
 
 # ------------------------------------------------
 # part 1
 # ------------------------------------------------
+b = Board(INPUT)
+b.plot()
+while not b.finished:
+    b.tick()
+print(b.score)
 
 # ------------------------------------------------
 # part 2
